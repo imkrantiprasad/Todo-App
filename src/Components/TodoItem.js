cb8839cb7ae5cf59a05d0e830d3ce8d1b842c903
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-08 18:02:10 +0530

    updates

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TodoItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Para 1</p>
+        </div>
+    )
+}
