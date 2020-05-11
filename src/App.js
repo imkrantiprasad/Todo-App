eaf7887212d8dba37ff43cb1ee8b754341e3caa8
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-11 21:57:28 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoItem from './Components/TodoItem'
 import todoData from './todoData'
 
 function App() {
-  const todoItems = todoData.map(item => <TodoItem key={item.id} text={item.text} />)
+  const todoItems = todoData.map(item => <TodoItem key={item.id} text={item.text} completed={item.completed} />)
   return (
     <div className="todo-list">
       {todoItems}
