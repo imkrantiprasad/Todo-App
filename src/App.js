d9590bf5b0b2518eb0877b8d1edaef01f83aa557
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-13 17:27:13 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './Components/TodoItem'
 import todoData from './todoData'
 
-function App() {
-  const todoItems = todoData.map(item => <TodoItem key={item.id} text={item.text} completed={item.completed} />)
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+  }
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} text={item.text} completed={item.completed} />)
+    return (
+      <div className="todo-list" >
+        {todoItems}
+      </div>
+    );
+
+  }
 }
 
 export default App;
