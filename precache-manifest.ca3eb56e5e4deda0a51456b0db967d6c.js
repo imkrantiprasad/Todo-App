faf8eee06822edb7a78f778484d4214efca27e73
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-23 18:42:59 +0530

    Updates

---
+++ precache-manifest.ca3eb56e5e4deda0a51456b0db967d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c742e87a6927cce3d201a14a2d3c5379",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "8bfec76f02924b45a329",
+    "url": "/Todo-App/static/css/main.00c95ca4.chunk.css"
+  },
+  {
+    "revision": "b9fa2aeeedc0a6371f86",
+    "url": "/Todo-App/static/js/2.941fb434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.941fb434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfec76f02924b45a329",
+    "url": "/Todo-App/static/js/main.536ce428.chunk.js"
+  },
+  {
+    "revision": "8d56a07d74d7b3d6cf0c",
+    "url": "/Todo-App/static/js/runtime-main.2e24af9b.js"
+  }
+]);
